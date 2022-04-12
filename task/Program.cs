6a7b3847ae89a4013208429412b056fa708f136c
Mihael Novitsky <mvn-tlt@mail.ru>
2022-04-12 21:42:57 +0400

    first commit

---
+++ task/Program.cs
@@ -0,0 +1,32 @@
+﻿
+string[] value = { "15", "hello", "600", "4", "control Work", "by", "Annaeva" };
+void Examination(string[] values)
+{
+    int count = 0;
+    int j = 0;
+    for (int i = 0; i < values.Length; i++)
+    {
+        string number = values[i];
+        if (number.Length <= 3)
+        {
+            count++;
+        }
+
+    }
+    string[] valuesTwo = new string[count];
+    
+    for (int i = 0; i < values.Length; i++)
+    {
+        
+        string number = values[i];
+        if (number.Length <= 3)
+        {
+            valuesTwo[j] = number;
+            Console.Write($"{valuesTwo[j]} ");
+            j++;
+        }
+
+    }
+
+}
+Examination(value);
